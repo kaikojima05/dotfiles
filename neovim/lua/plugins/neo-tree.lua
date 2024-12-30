dbe874f2f445101acb2f1d55c613c7358f574d8e
kaikojima <kojimakai5335@gmail.com>
2024-12-30 19:05:01 +0900

    neo-tree の設定変更した

--- neovim/lua/plugins/neo-tree.lua
+++ neovim/lua/plugins/neo-tree.lua
@@ -1,21 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   cmd = "Neotree",
-  opts = {
-    default_component_configs = {
-      keymap = {
-        ["<BS>"] = false, -- バックスペースキーのマッピングを無効化
-      },
-    },
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = false,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-      },
-    },
-  },
   config = function()
     require("neo-tree").setup({
       window = {
@@ -23,6 +8,19 @@ return {
         mappings = {
           ["h"] = "close_node",
           ["l"] = "open",
+        },
+      },
+      default_component_configs = {
+        keymap = {
+          ["<BS>"] = false, -- バックスペースキーのマッピングを無効化
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = false,
+          hide_dotfiles = false,
+          hide_gitignored = false,
         },
       },
     })

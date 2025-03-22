dfffd6192682d1f7f6cb29d438a644e04972d6b1
kaikojima <kojimakai5335@gmail.com>
2025-03-23 01:31:03 +0900

    nvim の設定ファイル更新

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,63 +1,27 @@
 return {
-	-- messages, cmdline and the popupmenu
 	{
+		-- プラグイン指定
 		"folke/noice.nvim",
+		-- opts フィールドとして関数を渡す
 		opts = function(_, opts)
+			-- すでにある opts.routes = {} がなければ作る
+			opts.routes = opts.routes or {}
+
+			-- コマンドライン以外のイベントは全部スキップ
 			table.insert(opts.routes, {
 				filter = {
-					event = "notify",
-					find = "No information available",
+					cond = function(details)
+						return details.event ~= "cmdline"
+					end,
 				},
 				opts = { skip = true },
 			})
-			local focused = true
-			vim.api.nvim_create_autocmd("FocusGained", {
-				callback = function()
-					focused = true
-				end,
-			})
-			vim.api.nvim_create_autocmd("FocusLost", {
-				callback = function()
-					focused = false
-				end,
-			})
-			table.insert(opts.routes, 1, {
-				filter = {
-					cond = function()
-						return not focused
-					end,
-				},
-				view = "notify_send",
-				opts = { stop = false },
-			})
-
-			opts.commands = {
-				all = {
-					-- options for the message history that you get with `:Noice`
-					view = "split",
-					opts = { enter = true, format = "details" },
-					filter = {},
-				},
-			}
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "markdown",
-				callback = function(event)
-					vim.schedule(function()
-						require("noice.text.markdown").keys(event.buf)
-					end)
-				end,
-			})
-
-			opts.presets.lsp_doc_border = true
 		end,
 	},
 
 	{
 		"rcarriga/nvim-notify",
-		opts = {
-			timeout = 7000,
-		},
+		enabled = false,
 	},
 
 	-- buffer line
@@ -76,7 +40,7 @@ return {
 	-- filename
 	{
 		"b0o/incline.nvim",
-    enabled = false,
+		enabled = false,
 	},
 
 	-- statusline

a374b5d09a7882911b2acad9a9753281ce0711ce
kaikojima <kojimakai5335@gmail.com>
2025-07-11 10:32:49 +0900

    neovim 起動中にインラインが表示されるようにした

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -179,6 +179,44 @@ return {
 		enabled = false, -- incline.nvimを無効化
 	},
 
+	-- mini.indentscope for showing current scope only
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		event = "LazyFile",
+		opts = {
+			symbol = "│",
+			options = { 
+				try_as_border = true,
+				border = "both",
+			},
+			draw = {
+				delay = 0,
+				animation = function() return 0 end,
+			},
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = {
+					"help",
+					"alpha",
+					"dashboard",
+					"neo-tree",
+					"Trouble",
+					"trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"lazyterm",
+				},
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+		end,
+	},
+
 	-- gitsigns for showing git diff indicators
 	{
 		"lewis6991/gitsigns.nvim",
